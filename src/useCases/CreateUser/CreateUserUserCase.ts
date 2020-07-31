ff785ec5eb6809c5113130f765dd1a1d4a06e4c3
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-07-30 22:50:51 -0300

    execute sendMail

--- src/useCases/CreateUser/CreateUserUserCase.ts
+++ src/useCases/CreateUser/CreateUserUserCase.ts
@@ -1,10 +1,12 @@
 import { ICreateUserRequestDTO } from './CreateUserDTO';
 import { IUsersRepository } from './../../repositories/IUsersRepository';
 import { User } from '../../entities/User';
+import { IMailProvider } from '../../providers/IMailProvider';
 export class CreateUserUseCase {
     
     constructor(
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider,
     ) { }
     
     async execute(data: ICreateUserRequestDTO) {
@@ -17,5 +19,18 @@ export class CreateUserUseCase {
         const user = new User(data)
 
         await this.usersRepository.save(user)
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email
+            },
+            from: {
+                name: 'Equipe',
+                email: 'teste@teste.com'
+            },
+            subject: 'Seja Bem Vindo',
+            body: '<p>Teste</p>'
+        })
     }
 }
